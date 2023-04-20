21ed8484ed88d77fc5ab8869083022b23be8f3f3
Vignesh <Vignesh@telebright.com>
2023-04-20 20:37:07 +0530

    Java version changed

--- src/main/java/com/example/demo/ZblSpringbootApplication.java
+++ src/main/java/com/example/demo/ZblSpringbootApplication.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ZblSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZblSpringbootApplication.class, args);
 	}
 
-	@GetMapping("/welcome")
-	public String welcome(){
-		return "Hello world... Welcome to Jenkins";
-	}
+	
 }
